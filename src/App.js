35b6eb6aff7778188cfd00176610f057a27114fd
liuchenghu <liuchenghu028@hellobike.com>
2024-12-12 11:13:08 +0800

    这是第二次修改，重新提交下

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>这是新加入的一行</div>
+        <div>之前修改文件错了，我重新提交下</div>
         <a
           className="App-link"
           href="https://reactjs.org"
