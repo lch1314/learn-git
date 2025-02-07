55972727d91ab79f4c7c01549781794b769e71d3
liuchenghu <liuchenghu028@hellobike.com>
2025-02-07 10:56:47 +0800

    ceshi

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <div>我是别的分支上合并过来的代码，并提前合并到master分支上了</div>
         <div>我是dev分支上做的修改</div>
         <div>我是dev2分支做的修改</div>
+        <div>cehsi</div>
         <a
           className="App-link"
           href="https://reactjs.org"
